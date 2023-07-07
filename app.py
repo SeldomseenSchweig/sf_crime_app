b85cc5c4c0fd2aca2b9e033d4135b75d5f527081
SeldomseenSchweig <jamesschweig@gmail.com>
2023-07-06 17:53:31 -0700

    tried to fix formatting

--- app.py
+++ app.py
@@ -139,20 +139,20 @@ def homepage():
     
 
    if g.user:
-    if g.user.location != "All":
-        resp = requests.get(f'{API_BASE_URL}analysis_neighborhood={g.user.location}')
-    else:
-        resp = requests.get(f'{API_BASE_URL}')
+       if g.user.location != "All":
+           resp = requests.get(f'{API_BASE_URL}analysis_neighborhood={g.user.location}')
+        else:
+            resp = requests.get(f'{API_BASE_URL}')
 
     data = json.loads(resp.text)
 
     messages = data[:10]
     converted_messages = convert(messages)
     processed_messages = intersection(converted_messages)
-
+    
     return render_template('home.html', messages=processed_messages)
-
-else:
+    
+    else:
     return render_template('home-anon.html')
 
 def convert(messages):
